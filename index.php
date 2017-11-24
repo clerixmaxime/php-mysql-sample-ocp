d35216d372ee3a83dc2cf9c6c4191e11f666910c
Maxime Clerix <mclerix@MacBook-Pro-de-Maxime.local>
2017-11-24 16:15:25 +0100

    Modification JRéDA

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
     <nav>
         <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">Red Hat OpenShift Démonstration</a>
+        <a href="#" class="brand-logo center">Inscription Journée JRéDA</a>
         <ul id="nav-mobile" class="left hide-on-med-and-down">
             <li><a href="welcome.php">Home</a></li>
             <li><a href="index.php">Register</a></li>
