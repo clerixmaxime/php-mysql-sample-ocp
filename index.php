e9b7a1ba1001bbf20c24abc942018d00dc2eba2f
Maxime Clerix <mclerix@MacBook-Pro-de-Maxime.local>
2017-11-27 10:13:57 +0100

    Titre

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
     <nav>
         <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">Inscription</a>
+        <a href="#" class="brand-logo center">Inscription Journées JRéda</a>
         <ul id="nav-mobile" class="left hide-on-med-and-down">
             <li><a href="welcome.php">Accueil</a></li>
             <li><a href="index.php">S'inscrire</a></li>
